04643e8682c3ee0ad19f5953f7226ea55d52fb99
Lynn Matini <40525115+Lynn-Matini@users.noreply.github.com>
2024-09-25 19:16:20 +0300

    Delete wema directory

--- wema/src/components/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-// eslint-disable-next-line react/prop-types
-function PrivateRoute({ children }) {
-  return localStorage.getItem('valid') ? children : <Navigate to="/" />;
-}
-
-export default PrivateRoute;
